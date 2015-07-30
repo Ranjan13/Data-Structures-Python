661fe7a94bfb5f4847064edb46a03ba8aac4a2cf
Ranjan Satapathy <kumarsatpathy@gmail.com>
2015-07-30 09:36:28 +0530

    Update stack-using-list.py

--- stack-using-list.py
+++ stack-using-list.py
@@ -1,31 +1,31 @@
 class Stack:
-        def __init__(self,limit = 1000):
-                self.stk = list()
-                self.limit = limit
-        def isEmpty(self):
-                return len(self.stk <= 0)
-        def StackPush(self,item):
-                if (len(self.stk) >= self.limit):
-                        print "Stack Overflow"
-                        return 0
-                else:
-                        self.stk.append(item)
-        def StackPop(self):
-                if (len(stack.stk) <= 0):
-                        print "Stack Underflow"
-                        return 0
-                else:
-                        self.stk.pop()
-        def size(self):
-                return len(self.stk)
-        def printStack(self):
-                if(len(self.stk) <= 0):
-                        print "Underflow"
-                        return 0
-                else:
-                        self.stk[-1]
-st = Stack(100)
-print st.printStack()
+	def __init__(self,limit = 10):
+		self.stk = list()
+		self.limit = limit
+	def isEmpty(self):
+		return len(self.stk <= 0)
+	def StackPush(self,item):
+		if (len(self.stk) >= self.limit):
+			print "Stack Overflow"
+			return 0
+		else:
+			self.stk.append(item)
+	def StackPop(self):
+		if (len(self.stk) <= 0):
+			print "Stack Underflow"
+			return 0
+		else:
+			self.stk.pop()
+	def size(self):
+		return len(self.stk)
+	def printStack(self):
+		if(len(self.stk) <= 0):
+			print "Yay!!, Underflow"
+			return 0
+		else:
+			for i in range(1,(len(self.stk))+1):
+				print self.stk[-i]
+st = Stack(5)
 st.StackPush("2")
 st.StackPush("1")
 st.StackPush("12")
