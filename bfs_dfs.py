82c0ba254b1f10364dee4c4de091094b0eb992f3
Ranjan Satapathy <kumarsatpathy@gmail.com>
2015-08-13 11:16:33 +0530

    Update bfs_dfs.py
    Updated version of bfs_dfs

--- bfs_dfs.py
+++ bfs_dfs.py
@@ -1,19 +1,11 @@
+
 import sys
+import time
 '''
-def expand(node):
-	try:
-		choice = int(raw_input("Enter 1 for BFS and 2 for DFS"))
-		if(choice == 1):
-			bfs(node)
-		elif(choice == 2):
-			dfs(node)
-	except ValueError:
-		print "Wrong entry"
+Expansion of each node is done here
 '''
 def expand(adj_list,q,e,root):
-	#m = adj_list[0][0]
 	q.append(root)
-	#e.append(root)
 	if sys._getframe().f_back.f_code.co_name == 'bfs':
 		for i in range(len(adj_list)):
 			for j in range(len(adj_list[i])):
@@ -26,27 +18,18 @@ def expand(adj_list,q,e,root):
 
 	elif sys._getframe().f_back.f_code.co_name == 'dfs':
 	#This one is for DFS
-		while(not(q == None)):
+		while( len(q) > 0 ):
 			print "Q is",q
 			if not(len(q) == 0):
 				n = q.pop()
 			print"After pop",q
-			#for i in range(len(adj_list[n-1])):
+			for i in range(len(adj_list[n-1])):
 			#print "i is",i
 				if n not in e:
 					q.append(adj_list[n-1][i])
 			if n not in e:
 				e.append(n)
 			print "Hola! ",e
-
-			#q.append(adj_list[n][ range(len(adj_list[n]))])
-			#if n not in e:
-			#	e.append(adj_list[n])
-			#	print "Hola! ",e
-
-			
-		#print len(q)
-		#for i in range(len(q),0,-1):	
 		print e
 def bfs(adj_list):
 	print adj_list
@@ -56,42 +39,40 @@ def bfs(adj_list):
 	expand(adj_list,q,e,root)
 	print "The BFS is:"
 	print e
+		
 def dfs(adj_list):
 	q,e = list(),list()
 	root = int(raw_input("Root:"))
 	expand(adj_list,q,e,root)
 	print "DFS is"
 	print e
-	
+		
 '''
 The following lines are to get the adjacency list
 '''
-
 print "Enter the number of nodes"
 node = int(raw_input())
 adj_list = [[0 for x in (range(node))] for x in (range(node))]
 print adj_list
 for i in range(len(adj_list)):
-	#for j in range(len(adj_list)):
-	#print i
-	adj_list[i] = map(int,raw_input().split())
-#print adj_list[]
+		adj_list[i] = map(int,raw_input().split())
+start_time_bfs = time.time()
 bfs(adj_list)
+end_time_bfs = time.time()
+total_bfs_time = end_time_bfs - start_time_bfs
+print "and it takes: ", total_bfs_time
+'''
+DFS is called
+'''
+start_time_dfs = time.time()
 dfs(adj_list)
+end_time_dfs = time.time()
+total_dfs_time =  end_time_dfs - start_time_dfs
+print "and it takes: ",total_dfs_time
 '''
-
-elif sys._getframe().f_back.f_code.co_name == 'dfs':
-	#This one is for DFS
-		for i in range(len(adj_list)):
-			for j in range(len(adj_list[i])):
-				if adj_list[i][j] not in q:
-					q.append(adj_list[i][j])
-			print "Q is",q
-			n = q.pop()
-			print"After pop",q
-			if n not in e:
-				e.append(n)
-				print "Hola! ",e
-
-
+Calculation for which Algorithm is faster
 '''
+if ((total_bfs_time - total_dfs_time) > 0):
+	print "DFS is faster than BFS"
+else:
+	print"BFS is faster than DFS"
